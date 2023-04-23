5a594332d7edb86d6d49f20c6149ff28a1125c51
RadhaNinave <radhikarao245@gmail.com>
2023-04-23 16:16:27 +0530

    adding cart model

--- Controllers/shop.js
+++ Controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart')
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -19,6 +19,13 @@ exports.getProduct = (req,res,next)=>{
   })
   })
  
+}
+exports.postCart = (req,res,next)=>{
+  const prodId=req.body.productId;
+  Product.findById(prodId,(product)=>{
+    Cart.addProduct(prodId,product.price)
+  })
+  res.redirect('/cart');
 }
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
